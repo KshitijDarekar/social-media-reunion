f69b3560b4fe4fbda70fe4ded9090ae8802620ab
Kshitij Darekar <kshitijdarekar13@gmail.com>
2023-01-18 01:22:37 +0530

    setup:nodejs-express server and mongodb connection

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World, from express.App is running !");
+});
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log("Error = " + err.message);
+  });
+
+//Start server
+const port = process.env.PORT || 3000;
+const ip = process.env.IP || "0.0.0.0";
+
+app.listen(port, ip, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+export default app;
